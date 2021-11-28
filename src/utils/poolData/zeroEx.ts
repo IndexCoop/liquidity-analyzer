0a59e0ac9107525275190a2b42b2427e971a2dbe
Richard Guan <guan.richard1@gmail.com>
2021-11-29 00:18:51 +1100

    .

--- src/utils/poolData/zeroEx.ts
+++ src/utils/poolData/zeroEx.ts
@@ -1,4 +1,5 @@
 import { BigNumber, constants } from "ethers";
+import { ZERO } from "utils/constants/constants";
 import { LiquidityBalance, MaxTradeResponse } from "./types";
 
 const mainNet = "https://api.0x.org";
@@ -6,9 +7,8 @@ const pricePath = "swap/v1/price";
 
 type ZeroExQuote = {
     price: string,
-    gas: string,
-    value: string,
     buyAmount: string,
+    buyTokenToEthRate: string,
 }
 
 /**
@@ -27,20 +27,39 @@ export async function getZeroExLiquidity(tokenAddress: string): Promise<Liquidit
  * @returns 
  */
 export async function getZeroExQuote(tokenAddress: string, maxSlippagePercent: number): Promise<MaxTradeResponse> {
-    const requestUrl = `${mainNet}/${pricePath}?`
-        + `sellToken=ETH&`
-        + `sellAmount=${constants.WeiPerEther.mul(1)}&`
-        + `buyToken=${tokenAddress}&`
-        + `slippagePercentage=${maxSlippagePercent / 100}`;
+    let slippage = 0;
+    let quote = await getQuote(tokenAddress, "WETH", 1, maxSlippagePercent);;
+    // let sellAmount = 1;
+    // while (slippage < maxSlippagePercent) {
+    //     quote = await getQuote(tokenAddress, "WETH", sellAmount, maxSlippagePercent);
+    //     slippage = (parseFloat(quote.buyTokenToEthRate) - parseFloat(quote.price)) / parseFloat(quote.buyTokenToEthRate);
+    //     sellAmount += 10;
+    //     await new Promise(f => setTimeout(f, 10000));
+    // }
+    // if (quote == null) {
+    //     throw new Error('No quote retrieved');
+    // }
 
+    if (BigNumber.from(quote.buyAmount).eq(0)) {
+        return {
+            size: BigNumber.from(constants.Zero),
+        }
+    }
+    return {
+        size: BigNumber.from(quote.buyAmount),
+    }
+}
+
+async function getQuote(buyToken: string, sellToken: string, sellAmount: number, slippagePercent: number): Promise<ZeroExQuote> {
+    const requestUrl = `${mainNet}/${pricePath}?`
+        + `sellToken=${sellToken}&`
+        + `sellAmount=${constants.WeiPerEther.mul(sellAmount)}&`
+        + `buyToken=${buyToken}&`
+        + `slippagePercentage=${slippagePercent / 100}`;
     console.log(requestUrl);
     const res = await fetch(requestUrl);
     if (!res.ok) {
         throw new Error('Failed to retrieve quote')
     }
-    const body: ZeroExQuote = await res.json();
-    console.log(body);
-    return {
-        size: BigNumber.from(body.buyAmount),
-    }
+    return await res.json();
 }
